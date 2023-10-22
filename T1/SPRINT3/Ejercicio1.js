1bdcd3e0efd4b15460c9730bfe4306866128f3e0
JoseAntonioSegura <jaseggom@adaits.es>
2023-10-22 12:29:59 +0200

    Ejercicios

---
+++ T1/SPRINT3/Ejercicio1.js
@@ -0,0 +1,20 @@
+//Variables
+var entrada = {a: 1, b: 2, c: 3, d: 4};
+var propiedades = ["b", "c"];
+
+//Función
+function filtrarPropiedades(obj, propiedades) {
+    //Creo un nuevo objeto que almacene los resultados.
+    let nuevoObjeto = {};
+
+    //Recorre la longitud de las propiedades
+    for (let i = 0; i < propiedades.length; i++) {
+        //Verifica si la propiedad se encuentra en el objecto
+        if (propiedades[i] in obj) {
+            nuevoObjeto[propiedades[i]] = obj[propiedades[i]];
+        }
+    }
+    return nuevoObjeto;
+}
+
+console.log(filtrarPropiedades(entrada, propiedades));
