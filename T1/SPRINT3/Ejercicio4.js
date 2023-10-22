3cd66005a6eea50c818eea6b59334ed85f2cf191
JoseAntonioSeguraGomez <115014384+JoseAntonioSegura@users.noreply.github.com>
2023-10-22 12:54:43 +0200

    Create Ejercicio4.js

---
+++ T1/SPRINT3/Ejercicio4.js
@@ -0,0 +1,17 @@
+var arr = ['pepe', 'manolo', 'paco', 'manuel'];
+
+function cadenaMasLarga(arr) {
+    let cadenaLarga = '';
+    let comparador = 0;
+
+    for(let i = 0; i < arr.length; i++) {
+        if (comparador < arr[i].length) {
+            comparador = arr[i].length;
+            cadenaLarga = arr[i];
+        }
+    }
+
+    return 'La cadena mas larga es la de ' + cadenaLarga + ' con una longitud de ' + comparador; 
+}
+
+console.log(cadenaMasLarga(arr));
