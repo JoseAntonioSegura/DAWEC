37c1efada9e9d4300e0735a1c1ce70c7ac9b73ef
JoseAntonioSeguraGomez <115014384+JoseAntonioSegura@users.noreply.github.com>
2024-01-14 21:42:10 +0100

    Si

--- T2/SPRINT 1/Ejercicio2/my-app/src/index.js
+++ T2/SPRINT 1/Ejercicio2/my-app/src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom/client";
 import App from './components/App';
+<<<<<<< Updated upstream
+=======
+import reportWebVitals from "./reportWebVitals";
+>>>>>>> Stashed changes
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
+
+reportWebVitals();
